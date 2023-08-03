e93ae92681cce0a0c3d757953348c35bce783d78
JD Nafziger <nafziger10@gmail.com>
2023-08-03 16:27:40 -0500

    required goals completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,11 +2,49 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+function addItem(item) {
+    basket.push(item);
+    return true;
+}// end addItem();
+
+function listItems() {
+    if (basket.length === 0){
+        console.log('basket is currently empty');
+        return false;
+    }
+    // console.log(`Full list ${basket}\nItems line by line`);
+    // ^THIS^ was used to test full list for clarity instead of line by line.
+    for (let item of basket) {
+        console.log(item);
+    }
+}// end listItems
+
+function empty(){
+    basket = [];
+}
 
 
+addItem('banana');
+addItem('grapes');
+addItem('pineapple');
+addItem('cheese curds');
 
+listItems();
 
+empty();
+listItems();
 
+addItem('poptarts');
+addItem('cherry');
+addItem('pizza');
+addItem('whiskey');
+addItem('chicken');
+addItem('Doritos');
+
+listItems();
+
+console.log('(expect true)')
 
 
 
